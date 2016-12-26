41fcbb2425ecf721a90ef063b4c375895b3b7cd5
kevinw123 <kevinwong_895@hotmail.com>
2016-12-25 23:18:59 -0800

    adding register form

---
+++ register.php
@@ -0,0 +1,11 @@
+<?php require('core/init.php'); ?>
+
+<?php
+// Get Template and Assign Variables
+$template = new Template('templates/register.php');
+
+// Assign variables
+
+
+// Display Template
+echo $template;
---
+++ templates/includes/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to TalkingSpace</title>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo BASE_URI; ?>templates/css/bootstrap.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="<?php echo BASE_URI; ?>templates/css/custom.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">TalkingSpace</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="register.html">Create An Account</a></li>
+            <li><a href="create.html">Create Topic</a></li>
+          </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="main-col">
+              <div class="block">
+                <h1 class="pull-left">Welcome to Talkingspace</h1>
+                <h4 class="pull-right">A simple PHP forum engine</h4>
+                <div class="clearfix"></div>
+                <hr>
