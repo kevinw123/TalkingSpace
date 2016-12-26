933a3c9b0bfe69915cd40a88cf87387e27c97a38
kevinw123 <kevinwong_895@hotmail.com>
2016-12-25 19:19:35 -0800

    Config file for database

--- libraries/Database.php
+++ libraries/Database.php
@@ -17,7 +17,7 @@ class Database {
 				PDO::ATTR_PERSISTENT => true,
 				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
 		);
-		// Create a new PDO instanace
+		// Create a new PDO instance
 		try {
 			$this->dbh = new PDO ($dsn, $this->user, $this->pass, $options);
 		}		// Catch any errors
